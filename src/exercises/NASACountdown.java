b5b6c77c79f81fd5b40205f7a0659f2e5f8e4c8f
IvanaDjikic <ivdjikic@gmail.com>
2019-04-25 00:29:23 +0200

    my changes 1

--- src/exercises/NASACountdown.java
+++ src/exercises/NASACountdown.java
@@ -1,11 +1,15 @@
 package exercises;
 
-/*
- * Count down a rocket launch!
- * 
- * Print the numbers from 10 to 1, then print “blastoff”. 
- */
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 public class NASACountdown {
+	public static void main(String[] args) {
+		System.out.println("A rocket is launched. The countdown is starting:");
+		for (int i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
 
+		System.out.println("blastoff");
+
+	}
 }
